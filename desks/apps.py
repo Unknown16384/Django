54e32e93af72e4a3547bf5420f9018333e740bde
Unknown16384 <kuromizu@ya.ru>
2025-08-27 23:28:07 +0000

    theme 06

--- desks/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DesksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'desks'
-    verbose_name = 'Рабочие места'
